5c7f82df488f156de364638220165a989d52decb
jeromie <jeromie@ajency.in>
2018-02-05 14:14:42 +0530

    else condition

--- wp-content/themes/HighendWP-child/scripts/custom.js
+++ wp-content/themes/HighendWP-child/scripts/custom.js
@@ -14,5 +14,8 @@ jQuery('body').on('click', '.pricing-tabs a', function(){
 	if (jQuery(this).attr('href')=='#booking-engine-tab'){
 		jQuery('.booking-engine-heading').siblings('.pricing-tabs-heading').addClass('hidden');
 		jQuery('.booking-engine-heading').removeClass('hidden');
+	} else if (jQuery(this).attr('href')=='#channel-manager-tab'){
+		jQuery('.channel-manager-heading').siblings('.pricing-tabs-heading').addClass('hidden');
+		jQuery('.channel-manager-heading').removeClass('hidden');
 	}
 });
