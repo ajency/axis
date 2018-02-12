dec59bbe3d1bb75d0593371c6077ccf610b14c3b
jeromie <jeromie@ajency.in>
2018-02-12 20:40:34 +0530

    svg tooltip

--- wp-content/themes/HighendWP-child/scripts/custom.js
+++ wp-content/themes/HighendWP-child/scripts/custom.js
@@ -115,35 +115,22 @@ jQuery('body').on('keydown', 'input, textarea', function(){
 
 // });
 
-// var tooltip = document.querySelector('.country-name');
-// var myicon = document.querySelector('.enabled');
-// var iconPos = myicon.getBoundingClientRect();
-// var colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"];
-// var contents = {};
-// for (var i = 1; i <= 10; i++) {
-// contents['tooltip' + i] = 'tooltip <b style="color:' + colors[i] + '">' + i + '</b>';
-// }
 
-// // iterate throw all `path` tags
-// [].forEach.call(document.querySelectorAll('g.enabled'), function(item) {
-// 	// attach click event, you can read the URL from a attribute for example.
-//   item.addEventListener('click', function(){
-//     window.open('http://google.co.il')
-//   });
 
-//   // attach mouseenter event
-//   item.addEventListener('mouseenter', function() {
-//     tooltip.innerHTML = contents[item.getAttribute('data-name')];
-//     tooltip.style.display = 'block';
-//   });
-
-//   item.addEventListener('mousemove', function(e) {
-//   	tooltip.style.top = (window.scrollY + iconPos.top - 60) + "px";
-//     tooltip.style.left = e.clientX + 'px';
-//   });
-
-//   // when mouse leave hide the tooltip
-//   item.addEventListener('mouseleave', function(){
-//   	tooltip.style.display = 'none';
-//   });
-// });
+var timeout;
+jQuery(".enabled").hover(
+  function() {
+    clearTimeout(timeout);
+    jQuery('.country-name').css({
+      'display':'block',
+      'position':'fixed',
+      'top':jQuery(this).offset().top - jQuery(window).scrollTop() - 40,
+      'left':jQuery(this).offset().left - 35
+    });
+    jQuery('.country-name').html(jQuery(this).data('name'));
+  },
+  function(){
+  	timeout = setTimeout(function(){
+    	jQuery('.country-name').css('display','none');
+      },1000);
+  });
