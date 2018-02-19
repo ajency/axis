b563ea3029418a6882c3400bc839174bd3a47f34
jeromie <jeromie@ajency.in>
2018-02-19 12:20:32 +0530

    doc changes

--- wp-content/themes/HighendWP-child/footer.php
+++ wp-content/themes/HighendWP-child/footer.php
@@ -101,6 +101,18 @@ if ( hb_options("hb_footer_bg_image") ){
 	</div>
 	<!-- END .container -->
 
+	<script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/57764bbd22723f0b61e178b7/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+
 </footer>
 <!-- END #footer -->
 <?php } } ?>
