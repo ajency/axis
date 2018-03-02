de1c349781df2629cbd639bacb68b1c8d8bf63cd
jeromie <jeromie@ajency.in>
2018-03-02 15:52:14 +0530

    pay styles

--- wp-content/themes/HighendWP-child/page-paynow.php
+++ wp-content/themes/HighendWP-child/page-paynow.php
@@ -19,37 +19,43 @@ if ( vp_metabox('background_settings.hb_content_background_color') )
 
 <div id="main-content"<?php echo $main_content_style; ?>>
 	<div class="container">
-		<form method="post" action="<?php echo get_stylesheet_directory_uri(); ?>/page-payform.php">
-		  <table width="100%" align="center" border="0" cellspacing="2" cellpadding="3" class="tdata">
-		    <tr>
-		      <td width="35%">Hotel Name:</td>
-		      <td><input type="text" name="name" id="name" required></td>
-		    </tr>
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<form method="post" action="<?php echo get_stylesheet_directory_uri(); ?>/page-payform.php">
+				  <table width="100%" align="center" border="0" cellspacing="2" cellpadding="3" class="tdata">
+				    <tr>
+				      <td width="35%">Hotel Name:</td>
+				      <td><input type="text" name="name" id="name" placeholder="Enter Hotel Name" required></td>
+				    </tr>
 
-		    <tr>
-		      <td>Email:</td>
-		      <td><input type="text" name="email" id="email" required></td>
-		    </tr>
-		    <tr>
-		      <td>Contact Number:</td>
-		      <td><input type="text" name="contact_no" id="contact_no" required></td>
-		    </tr>
-		    <tr>
-		      <td>Invoice:</td>
-		      <td><input type="text" name="company" id="company" required></td>
-		    </tr>
-		    <tr>
-		      <td>Amount:</td>
-		      <td><input type="number" name="amount" id="amount" value="0.0"></td>
-		    </tr>
+				    <tr>
+				      <td>Email:</td>
+				      <td><input type="text" name="email" id="email" placeholder="Enter Email Address" required></td>
+				    </tr>
+				    <tr>
+				      <td>Contact Number:</td>
+				      <td><input type="text" name="contact_no" id="contact_no" placeholder="Enter Contact Number" required></td>
+				    </tr>
+				    <tr>
+				      <td>Invoice:</td>
+				      <td><input type="text" name="company" id="company" placeholder="Enter Invoice Number" required></td>
+				    </tr>
+				    <tr>
+				      <td>Amount:</td>
+				      <td><input type="number" name="amount" id="amount" value="0"></td>
+				    </tr>
 
-		    <tr>
-		      <td>&nbsp;</td>
-		      <td><input type="submit" name="submit" value="&nbsp; Submit &nbsp;"></td>
-		    </tr>
+				    <tr>
+				      <td>&nbsp;</td>
+				      <td class="axis-green-btn">
+				      	<button type="submit" name="submit">Submit</button>
+				      </td>
+				    </tr>
 
-		  </table>
-		</form>
+				  </table>
+				</form>
+			</div>
+		</div>
 	</div>
 </div>
 
